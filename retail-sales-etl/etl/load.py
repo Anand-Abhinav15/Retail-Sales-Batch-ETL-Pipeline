72eea3ba46ab7d97674362d8d96ab2561ad0673b
Abhinav Anand <a8hinav.anand1508@gmail.com>
2026-06-07 23:54:36 +0530

    Added load_dim_products func in load.py

--- retail-sales-etl/etl/load.py
+++ retail-sales-etl/etl/load.py
@@ -26,9 +26,32 @@ def load_dim_product(df):
 
     engine = get_engine()
 
-    
+    products = (
+        df[["product_id"]].drop_duplicates()
+    )
 
+    with engine.begin() as conn:
 
+        for _, row in products.iterrows():
+
+            conn.exec_driver_sql(
+                f"""
+                INSERT INTO dim_product(product_id)
+                VALUES ({int(row['product_id'])})
+                ON CONFLICT(product_id)
+                DO NOTHING;
+                """
+            )
+
+def load_dim_store(df):
+     
+    engine = get_engine()
+
+    stores = (
+         df[["store_id", "region"]].drop_duplicates()
+    )
+
+    with engine.begin() as conn:s
 
 
 
